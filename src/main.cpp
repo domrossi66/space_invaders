ec823573c12a4fb78bf51d5aad2a7cb88b3f9407
Dominic <dominic.rossi@uconn.edu>
2026-01-11 18:16:36 -0500

    Raylib files added

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <raylib.h>
+
+int main() 
+{
+    int windowWidth = 750;
+    int windowHeight = 700;
+
+    InitWindow(windowWidth, windowHeight, "Space Invaders Project");
+
+    CloseWindow();
+}
