1d99a94a909e702b65571c76616ead5f0a3dd68e
Dominic <dominic.rossi@uconn.edu>
2026-01-12 16:18:05 -0500

    Added game loop

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,18 @@ int main()
 {
     int windowWidth = 750;
     int windowHeight = 700;
+    Color gray = {29, 29, 29, 255};
 
     InitWindow(windowWidth, windowHeight, "Space Invaders Project");
+    
+    SetTargetFPS(60);
+
+    while(WindowShouldClose() == false)
+    {
+        BeginDrawing();
+        ClearBackground(gray);
+        EndDrawing();
+    }
 
     CloseWindow();
 }
